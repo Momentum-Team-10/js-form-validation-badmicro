5e11d0c39cd2ae2bd6e407a0ecc4e39385c69221
Jeffrey <jrgilley92@gmail.com>
2021-10-11 21:16:45 -0400

    CVV Validation working

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function validateCVV (input) {
     let errorEl = document.createElement('p')
     errorEl.id = 'cvv-error'
 
-    if(input.length != 3 || input.typeof != "number") {
+    if(input.value.length != 3 || isNaN(input.value)) {
         if(document.getElementById('cvv-error')) {
             document.getElementById('cvv-error').remove()
         }
@@ -136,4 +136,27 @@ function validateCVV (input) {
         CVVInput.parentElement.classList.remove('input-invalid')
         CVVInput.parentElement.classList.add('input-valid')
     }
+}
+
+function validateCardNumber(number) {
+    var regex = new RegExp("^[0-9]{16}$");
+    if (!regex.test(number))
+        return false;
+
+    return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+    var sum = 0;
+    for (var i = 0; i < val.length; i++) {
+        var intVal = parseInt(val.substr(i, 1));
+        if (i % 2 == 0) {
+            intVal *= 2;
+            if (intVal > 9) {
+                intVal = 1 + (intVal % 10);
+            }
+        }
+        sum += intVal;
+    }
+    return (sum % 10) == 0;
 }
